e5de566e2129f52c1d3553183317bdaaa63c0bca
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2014-12-09 08:56:21 -0500

    Added board array

---
+++ Just-Get-10.js
@@ -0,0 +1,82 @@
+var boxes = {
+  rc11 = 0;
+  rc12 = 0;
+  rc13 = 0;
+  rc14 = 0;
+  rc15 = 0;
+  rc16 = 0;
+  rc17 = 0;
+  rc18 = 0;
+
+  rc21 = 0;
+  rc22 = 0;
+  rc23 = 0;
+  rc24 = 0;
+  rc25 = 0;
+  rc26 = 0;
+  rc27 = 0;
+  rc28 = 0;
+
+  rc31 = 0;
+  rc32 = 0;
+  rc33 = 0;
+  rc34 = 0;
+  rc35 = 0;
+  rc36 = 0;
+  rc37 = 0;
+  rc38 = 0;
+
+  rc41 = 0;
+  rc42 = 0;
+  rc43 = 0;
+  rc44 = 0;
+  rc45 = 0;
+  rc46 = 0;
+  rc47 = 0;
+  rc48 = 0;
+
+  rc51 = 0;
+  rc52 = 0;
+  rc53 = 0;
+  rc54 = 0;
+  rc55 = 0;
+  rc56 = 0;
+  rc57 = 0;
+  rc58 = 0;
+
+  rc61 = 0;
+  rc62 = 0;
+  rc63 = 0;
+  rc64 = 0;
+  rc65 = 0;
+  rc66 = 0;
+  rc67 = 0;
+  rc68 = 0;
+
+  rc71 = 0;
+  rc72 = 0;
+  rc73 = 0;
+  rc74 = 0;
+  rc75 = 0;
+  rc76 = 0;
+  rc77 = 0;
+  rc78 = 0;
+
+  rc81 = 0;
+  rc82 = 0;
+  rc83 = 0;
+  rc84 = 0;
+  rc85 = 0;
+  rc86 = 0;
+  rc87 = 0;
+  rc88 = 0;
+}
+
+function highlightBox(box, color) {
+
+}
+
+$(".box").click(function() {
+  var box = $(this).attr("id");
+
+})
